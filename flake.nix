6ccefd23121f008f9b0bb41fb25d8b74432f0f51
Oleksii Vasylenko <oleksii.vasylenko@howly.com>
2024-09-03 10:50:54 +0300

    Added gofumpt and git blame line keymap

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
       environment.systemPackages = with pkgs; [
         vim
         go
+        gofumpt
         zig
         fzf
         ripgrep
--- nvim/default.nix
+++ nvim/default.nix
@@ -65,6 +65,14 @@
       }
       {
         mode = ["n"];
+        key = "<leader>glb";
+        action = "<cmd>Gitsigns toggle_current_line_blame<CR>";
+        options = {
+          desc = "Blame current line";
+        };
+      }
+      {
+        mode = ["n"];
         key = "<leader>e";
         action = "<cmd>Oil<cr>";
         options = {desc = "Open file tree";};
@@ -654,9 +662,11 @@
           formatting = {
             goimports.enable = true;
             gofmt.enable = true;
+            # buf.enable = true;
+            gofumpt.enable = true;
           };
           diagnostics = {
-            # buf.enable = true;
+            buf.enable = true;
             # golangci_lint.enable = true;
           };
         };
@@ -725,128 +735,129 @@
       };
       conform-nvim = {
         enable = true;
-        formatOnSave = ''
-          function(bufnr)
-            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-              return
-            end
-
-            if slow_format_filetypes[vim.bo[bufnr].filetype] then
-              return
-            end
-
-            local function on_format(err)
-              if err and err:match("timeout$") then
-                slow_format_filetypes[vim.bo[bufnr].filetype] = true
+        settings = {
+          format_on_save = ''
+            function(bufnr)
+              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
               end
+
+              if slow_format_filetypes[vim.bo[bufnr].filetype] then
+                return
+              end
+
+              local function on_format(err)
+                if err and err:match("timeout$") then
+                  slow_format_filetypes[vim.bo[bufnr].filetype] = true
+                end
+              end
+
+              return { timeout_ms = 200, lsp_fallback = true }, on_format
+             end
+          '';
+
+          format_after_save = ''
+            function(bufnr)
+              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+              end
+
+              if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+                return
+              end
+
+              return { lsp_fallback = true }
             end
-
-            return { timeout_ms = 200, lsp_fallback = true }, on_format
-           end
-        '';
-
-        formatAfterSave = ''
-          function(bufnr)
-            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-              return
-            end
-
-            if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-              return
-            end
-
-            return { lsp_fallback = true }
-          end
-        '';
-        notifyOnError = true;
-        formattersByFt = {
-          html = [
-            [
-              "prettierd"
-              "prettier"
-            ]
-          ];
-          css = [
-            [
-              "prettierd"
-              "prettier"
-            ]
-          ];
-          # javascript = [
-          #   [
-          #     "prettierd"
-          #     "prettier"
-          #   ]
-          # ];
-          go = ["gofmt" "goimports"];
-          typescript = [
-            [
-              "prettierd"
-              "prettier"
-            ]
-          ];
-          python = [
-            "black"
-            "isort"
-          ];
-          lua = ["stylua"];
-          nix = ["alejandra"];
-          markdown = [
-            [
-              "prettierd"
-              "prettier"
-            ]
-          ];
-          yaml = [
-            [
-              "prettierd"
-              "prettier"
-            ]
-          ];
-          terraform = ["terraform_fmt"];
-          bash = [
-            "shellcheck"
-            "shellharden"
-            "shfmt"
-          ];
-          json = ["jq"];
-          "_" = ["trim_whitespace"];
-        };
-
-        formatters = {
-          black = {
-            command = "${lib.getExe pkgs.black}";
+          '';
+          formatters_by_ft = {
+            html = [
+              [
+                "prettierd"
+                "prettier"
+              ]
+            ];
+            css = [
+              [
+                "prettierd"
+                "prettier"
+              ]
+            ];
+            # javascript = [
+            #   [
+            #     "prettierd"
+            #     "prettier"
+            #   ]
+            # ];
+            go = ["gofmt" "goimports"];
+            typescript = [
+              [
+                "prettierd"
+                "prettier"
+              ]
+            ];
+            python = [
+              "black"
+              "isort"
+            ];
+            lua = ["stylua"];
+            nix = ["alejandra"];
+            markdown = [
+              [
+                "prettierd"
+                "prettier"
+              ]
+            ];
+            yaml = [
+              [
+                "prettierd"
+                "prettier"
+              ]
+            ];
+            terraform = ["terraform_fmt"];
+            bash = [
+              "shellcheck"
+              "shellharden"
+              "shfmt"
+            ];
+            json = ["jq"];
+            "_" = ["trim_whitespace"];
           };
-          isort = {
-            command = "${lib.getExe pkgs.isort}";
+          notify_on_error = true;
+          formatters = {
+            black = {
+              command = "${lib.getExe pkgs.black}";
+            };
+            isort = {
+              command = "${lib.getExe pkgs.isort}";
+            };
+            alejandra = {
+              command = "${lib.getExe pkgs.alejandra}";
+            };
+            jq = {
+              command = "${lib.getExe pkgs.jq}";
+            };
+            prettierd = {
+              command = "${lib.getExe pkgs.prettierd}";
+            };
+            stylua = {
+              command = "${lib.getExe pkgs.stylua}";
+            };
+            shellcheck = {
+              command = "${lib.getExe pkgs.shellcheck}";
+            };
+            shfmt = {
+              command = "${lib.getExe pkgs.shfmt}";
+            };
+            shellharden = {
+              command = "${lib.getExe pkgs.shellharden}";
+            };
+            goftm = {
+              comand = "${lib.getExe pkgs.gofumpt}";
+            };
+            #yamlfmt = {
+            #  command = "${lib.getExe pkgs.yamlfmt}";
+            #};
           };
-          alejandra = {
-            command = "${lib.getExe pkgs.alejandra}";
-          };
-          jq = {
-            command = "${lib.getExe pkgs.jq}";
-          };
-          prettierd = {
-            command = "${lib.getExe pkgs.prettierd}";
-          };
-          stylua = {
-            command = "${lib.getExe pkgs.stylua}";
-          };
-          shellcheck = {
-            command = "${lib.getExe pkgs.shellcheck}";
-          };
-          shfmt = {
-            command = "${lib.getExe pkgs.shfmt}";
-          };
-          shellharden = {
-            command = "${lib.getExe pkgs.shellharden}";
-          };
-          goftm = {
-            comand = "${lib.getExe pkgs.gofumpt}";
-          };
-          #yamlfmt = {
-          #  command = "${lib.getExe pkgs.yamlfmt}";
-          #};
         };
       };
 

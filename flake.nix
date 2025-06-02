1b32c999966339e89cf6801bfe6bab7a58d139c7
Oleksii Vasylenko <oleksii.vasylenko@howly.com>
2025-06-02 10:48:33 +0300

    updated

--- flake.nix
+++ flake.nix
@@ -33,18 +33,12 @@
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [
         vim
-        # go
-        go_1_23
-        gofumpt
         zig
         fzf
         ripgrep
-        delve #debug adapter for golang
         kubectl
         k9s
         git
-        redli
-        redis-plus-plus
         lazygit
         libiconv
         (
@@ -54,10 +48,14 @@
               pip
             ])
         )
+        ###go
         golangci-lint
-        d2
+        # go
+        go_1_23
+        gofumpt
+        delve #debug adapter for golang
+        #
         mermaid-cli
-        hoppscotch
         tmux
         buf
         cargo
@@ -77,24 +75,11 @@
         bunyan-rs
         trivy
         _1password-cli
-        superfile
+        yazi
         protols
-        # (nixvim-olva.packages.${system}.default)
-        # (pkgs.rustPlatform.buildRustPackage rec {  looks like it taking flit sources
-        #   pname = "protols";
-        #   version = "0.6.0";
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "coder3101";
-        #     repo = "protols";
-        #     rev = "993120b7ea68f015db8bef6912dbc8bb9098504e";
-        #     hash = "sha256-lLlad/kbrjwPE8ZdzebJMhA06AqpmEI+PJCWz12LYRM=";
-        #   };
-        #   cargoSha256 = "03wf9r2csi6jpa7v5sw5lpxkrk4wfzwmzx7k3991q3bdjzcwnnwp";
-        #   cargoLock = {
-        #     lockFile = "${src}/Cargo.lock";
-        #     # allowBuiltinFetchGit = true;
-        #   };
-        # })
+        hurl
+
+        (nixvim-olva.lib.makeNixvimWithExtra system ./nvim/project-specifix.nix)
       ];
 
       nixpkgs.config.allowUnfree = true;
@@ -105,9 +90,9 @@
       # };
 
       imports = [
-        nixvim.nixDarwinModules.nixvim
-        ./nvim
-        home-manager.darwinModules.home-manager
+        # nixvim.nixDarwinModules.nixvim
+        # ./nvim
+        # home-manager.darwinModules.home-manager
 
         {
           # home-manager = {
@@ -137,62 +122,15 @@
       nix.enable = true;
       # Auto upgrade nix package and the daemon service.
       programs = {
-        tmux = {
-          # plugins = with pkgs; [
-          #   tmuxPlugins.resurrect
-          #   tmuxPlugins.weather
-          #   tmuxPlugins.cpu
-          #   tmuxPlugins.better-mouse-mode
-          # ];
-          extraConfig = ''
-            resurrect_dir=${pkgs.tmuxPlugins.cpu}
-            set -g @resurrect-dir $resurrect_dir
-            set -g @resurrect-hook-post-save-all 'target=$(readlink -f $resurrect_dir/last); sed "s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g; s|/home/$USER/.nix-profile/bin/||g" $target | sponge $target'
-            # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
-            set -g default-terminal "xterm-256color"
-            set -ga terminal-overrides ",*256col*:Tc"
-            set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
-
-            set -gq allow-passthrough on
-
-            # Mouse works as expected
-            set-option -g mouse on
-            # easy-to-remember split pane commands
-            #bind | split-window -h -c "#{pane_current_path}"
-            #bind - split-window -v -c "#{pane_current_path}"
-            #bind c new-window -c "#{pane_current_path}"
-          '';
-        };
         zsh = {
           enable = true;
         };
 
-        nixvim.enable = true;
+        # nixvim.enable = true;
       };
       fonts.packages = with pkgs; [
         nerd-fonts.mononoki
       ];
-
-      # trying to override
-      # nixvim-olva = {
-      #   keymaps = [
-      #     {
-      #       mode = ["n" "v"];
-      #       key = "<leader>uid";
-      #       action.__raw = ''
-      #         function()
-      #            local uuid = vim.fn.system("uuidgen"):gsub("\n", "")
-      #            uuid = string.lower(uuid)
-      #            vim.api.nvim_put({uuid}, "", true, true)
-      #         end
-      #       '';
-      #       options = {
-      #         desc = "Put UUID";
-      #       };
-      #     }
-      #   ];
-      # };
-      # nix.package = pkgs.nix;
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
@@ -208,6 +146,7 @@
             autohide = true;
           };
         };
+        primaryUser = "user";
         stateVersion = 4;
         configurationRevision = self.rev or self.dirtyRev or null;
       };
@@ -218,8 +157,6 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
-
-      # nixpkgs.hostPlatform = "x86_64-darwin";
     };
   in {
     # Build darwin flake using:
--- nvim/project-specifix.nix
+++ nvim/project-specifix.nix
@@ -1,40 +1,46 @@
-_: {
-  programs.nixvim = {
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>fmt";
-        # action = "<cmd>!ghokin fmt replace .<CR>";
-        action.__raw = ''
-          function()
-             local filepath = vim.fn.expand('%:p')
-             local escaped_path = vim.fn.shellescape(filepath)
-             vim.cmd('!ghokin fmt replace ' .. escaped_path)
-          end
-        '';
-        options = {
-          desc = "Reformat";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>tb";
-        action.__raw = ''
-          function()
-           require('dap').run({
-             type = "go",
-             name = "TestAllSuites",
-             request = "launch",
-             mode = "test",
-             program = "./tests",
-             args = { "-test.run", "TestAllSuites", "-godog.tags", "@wip"},
-           })
-           end
-        '';
-        options = {
-          desc = "Debug bdd test";
-        };
-      }
-    ];
+{lib, ...}: {
+  plugins = {
+    fidget.enable = lib.mkForce false;
+    git-conflict.enable = true;
+    lsp.servers.cucumber_language_server = {
+      cmd = ["npx" "cucumber-language-server" "--stdio"];
+      enable = lib.mkForce false;
+    };
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>fmt";
+      # action = "<cmd>!ghokin fmt replace .<CR>";
+      action.__raw = ''
+        function()
+           local filepath = vim.fn.expand('%:p')
+           local escaped_path = vim.fn.shellescape(filepath)
+           vim.cmd('!ghokin fmt replace ' .. escaped_path)
+        end
+      '';
+      options = {
+        desc = "Reformat";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>tb";
+      action.__raw = ''
+        function()
+         require('dap').run({
+           type = "go",
+           name = "TestAllSuites",
+           request = "launch",
+           mode = "test",
+           program = "./tests",
+           args = { "-test.run", "TestAllSuites", "-godog.tags", "@wip"},
+         })
+         end
+      '';
+      options = {
+        desc = "Debug bdd test";
+      };
+    }
+  ];
 }
